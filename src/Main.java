517435cacfbb3deb055916505c07485bf48c8d3b
Beau Kesler <keslerbeau94@outlook.com>
2024-08-28 21:38:03 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void areaOfCircle()
+    {
+        int radius = 2;
+        double area = 3.14 * radius * radius;
+
+
+
+
+        System.out.println("The area of a circle is " + area);
+        ;
+
+    }
+
+    public static void main(String[] args) {
+        areaOfCircle();
+    }
+}
